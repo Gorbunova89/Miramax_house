8ade471f462e0d9b58981e49bed58da2bd85486f
Liudmila Gorbunova <ambersnake1989@gmail.com>
2023-02-10 00:51:07 +0300

    js finish

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,35 @@
 $(document).ready(function(){
     $('#showOptions').on('click', function() {
         let opt = document.getElementById('contentOptions');
-        if(opt.classList.contains("visually-hidden")){
-            opt.classList.remove("visually-hidden");
-        }else{
-            opt.classList.add("visually-hidden");
-        }
-       
-        
+        setVisually(opt); 
+
+        let icon = document.getElementById('showOptions');
+        setIcon(icon);
+    });
+
+    $('#showOptions2').on('click', function() {
+        let opt = document.getElementById('contentOptions2');
+        setVisually(opt);
+        let icon = document.getElementById('showOptions2');
+        setIcon(icon);
     });
 });
+
+function setVisually(opt) {
+    if (opt.classList.contains("visually-hidden")) {
+        opt.classList.remove("visually-hidden");
+    } else {
+        opt.classList.add("visually-hidden");
+    }
+};
+
+function setIcon(icon) {
+    if (icon.classList.contains("bi-arrow-down-square")) {
+        icon.classList.remove("bi-arrow-down-square");
+        icon.classList.add("bi-arrow-up-square");
+    } else{
+        icon.classList.remove("bi-arrow-up-square");
+        icon.classList.add("bi-arrow-down-square");
+    }
+
+};
